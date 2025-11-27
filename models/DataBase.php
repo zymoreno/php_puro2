dce1d8cd6d483e7ba0692498ac7bf7099e0cc0e3
zymoreno <zaida81@gmail.com>
2025-11-26 22:50:13 -0500

    validacion

--- models/DataBase.php
+++ models/DataBase.php
@@ -10,6 +10,7 @@
         //     $pdo = new PDO("mysql:host=$hostname;port=$port;dbname=$database;charset=utf8",$username,$password);
         //     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         //     return $pdo;
+        //
         // }
         
         ## Conexión Azure
