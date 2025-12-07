8778f6b5cdd781febf4b2db9e780bc7507cd5847
zymoreno <zaida81@gmail.com>
2025-12-06 21:22:27 -0500

    correccion

--- controllers/Login.php
+++ controllers/Login.php
@@ -1,39 +1,47 @@
 <?php
-    namespace App\Controllers;
+namespace App\Controllers;
 
-    use App\Models\User;
-    class Login{
-        private const LOGIN_VIEW = "views/company/login.view.php";
-        // Controlador Principal
-        public function main(){
-            if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-                if (empty($_SESSION['session'])) {
-                    require self::LOGIN_VIEW; 
-                } else {
+use App\Models\User;
+
+class Login {
+    private const LOGIN_VIEW = "views/company/login.view.php";
+
+    public function main() {
+
+        if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+
+            if (empty($_SESSION['session'])) {
+                require_once self::LOGIN_VIEW;
+            } else {
+                header(self::REDIRECT_DASHBOARD);
+            }
+        }
+
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+            $profile = new User(
+                $_POST['user_email'],
+                $_POST['user_pass']
+            );
+
+            $profile = $profile->login();
+
+            if ($profile) {
+                $active = $profile->getUserState();
+
+                if ($active != 0) {
+                    $_SESSION['session'] = $profile->getRolName();
+                    $_SESSION['profile'] = serialize($profile);
                     header(self::REDIRECT_DASHBOARD);
-                }
-            }
-            if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-                $profile = new User(
-                    $_POST['user_email'],
-                    $_POST['user_pass']
-                );
-                $profile = $profile->login();
-                if ($profile) {
-                    $active = $profile->getUserState();
-                    if ($active != 0) {
-                        $_SESSION['session'] = $profile->getRolName();
-                        $_SESSION['profile'] = serialize($profile);
-                        header(self::REDIRECT_DASHBOARD);
-                    } else {
-                        $message = "El Usuario NO está activo";
-                        require self::LOGIN_VIEW;
-                    }
                 } else {
-                    $message = "Credenciales incorrectas ó el Usuario NO existe";
-                    require self::LOGIN_VIEW;
+                    $message = "El Usuario NO está activo";
+                    require_once self::LOGIN_VIEW;
                 }
-            }
 
+            } else {
+                $message = "Credenciales incorrectas ó el Usuario NO existe";
+                require_once self::LOGIN_VIEW;
+            }
         }
     }
+}
