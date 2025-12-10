533a4461ec96c9bfe894af3a8c31b01b3565e2cb
zymoreno <zaida81@gmail.com>
2025-12-09 20:48:07 -0500

    index

--- views/company/header.view.php
+++ views/company/header.view.php
@@ -63,6 +63,7 @@ https://templatemo.com/tm-559-zay-shop
                 Zay
             </a>
 
+
             <button class="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#templatemo_main_nav" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
