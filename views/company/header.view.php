8fb1925e49263dc0efebc302889d09bff1bc7000
zymoreno <zaida81@gmail.com>
2025-12-13 22:06:22 -0500

    accessibility

--- views/company/header.view.php
+++ views/company/header.view.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
 <head>
     <title>Profe_Albeiro</title>
@@ -11,6 +11,7 @@
     <meta name="HandheldFriendly" content="true">
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+
     <link rel="shortcut icon" type="image/png" href="./img/ProgramadorFitness.png">
     <link rel="manifest" href="./manifest.json">
 
@@ -21,34 +22,51 @@
     <link rel="stylesheet" href="assets/landing/css/templatemo.css">
     <link rel="stylesheet" href="assets/landing/css/custom.css">
 
-    <!-- Load fonts style after rendering the layout styles -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;200;300;400;500;700;900&display=swap">
     <link rel="stylesheet" href="assets/landing/css/fontawesome.min.css">
-<!--
-
-TemplateMo 559 Zay Shop
-
-https://templatemo.com/tm-559-zay-shop
-
--->
+    <!--
+    TemplateMo 559 Zay Shop
+    https://templatemo.com/tm-559-zay-shop
+    -->
 </head>
 
 <body>
     <!-- Start Top Nav -->
-    <nav class="navbar navbar-expand-lg bg-dark navbar-light d-none d-lg-block" id="templatemo_nav_top">
+    <nav class="navbar navbar-expand-lg bg-dark navbar-light d-none d-lg-block" id="templatemo_nav_top" aria-label="Barra superior de contacto">
         <div class="container text-light">
             <div class="w-100 d-flex justify-content-between">
                 <div>
-                    <i class="fa fa-envelope mx-2"></i>
-                    <a class="navbar-sm-brand text-light text-decoration-none" href="mailto:info@company.com">info@company.com</a>
-                    <i class="fa fa-phone mx-2"></i>
-                    <a class="navbar-sm-brand text-light text-decoration-none" href="tel:010-020-0340">010-020-0340</a>
+                    <i class="fa fa-envelope mx-2" aria-hidden="true"></i>
+                    <a class="navbar-sm-brand text-light text-decoration-none" href="mailto:info@company.com">
+                        info@company.com
+                    </a>
+
+                    <i class="fa fa-phone mx-2" aria-hidden="true"></i>
+                    <a class="navbar-sm-brand text-light text-decoration-none" href="tel:010-020-0340">
+                        010-020-0340
+                    </a>
                 </div>
-                <div>
-                    <a class="text-light" href="https://fb.com/templatemo" target="_blank" rel="sponsored"><i class="fab fa-facebook-f fa-sm fa-fw me-2"></i></a>
-                    <a class="text-light" href="https://www.instagram.com/" target="_blank"><i class="fab fa-instagram fa-sm fa-fw me-2"></i></a>
-                    <a class="text-light" href="https://twitter.com/" target="_blank"><i class="fab fa-twitter fa-sm fa-fw me-2"></i></a>
-                    <a class="text-light" href="https://www.linkedin.com/" target="_blank"><i class="fab fa-linkedin fa-sm fa-fw"></i></a>
+
+                <div aria-label="Redes sociales">
+                    <a class="text-light" href="https://fb.com/templatemo" target="_blank" rel="sponsored"
+                       aria-label="Abrir Facebook (nueva pestaña)">
+                        <i class="fab fa-facebook-f fa-sm fa-fw me-2" aria-hidden="true"></i>
+                    </a>
+
+                    <a class="text-light" href="https://www.instagram.com/" target="_blank"
+                       aria-label="Abrir Instagram (nueva pestaña)">
+                        <i class="fab fa-instagram fa-sm fa-fw me-2" aria-hidden="true"></i>
+                    </a>
+
+                    <a class="text-light" href="https://twitter.com/" target="_blank"
+                       aria-label="Abrir Twitter (nueva pestaña)">
+                        <i class="fab fa-twitter fa-sm fa-fw me-2" aria-hidden="true"></i>
+                    </a>
+
+                    <a class="text-light" href="https://www.linkedin.com/" target="_blank"
+                       aria-label="Abrir LinkedIn (nueva pestaña)">
+                        <i class="fab fa-linkedin fa-sm fa-fw" aria-hidden="true"></i>
+                    </a>
                 </div>
             </div>
         </div>
@@ -56,21 +74,25 @@ https://templatemo.com/tm-559-zay-shop
     <!-- Close Top Nav -->
 
     <!-- Header -->
-    <nav class="navbar navbar-expand-lg navbar-light shadow">
+    <nav class="navbar navbar-expand-lg navbar-light shadow" aria-label="Navegación principal">
         <div class="container d-flex justify-content-between align-items-center">
 
-            <a class="navbar-brand text-success logo h1 align-self-center" href="index.php">
+            <a class="navbar-brand text-success logo h1 align-self-center" href="index.php" aria-label="Ir al inicio">
                 Zay
             </a>
 
-
-            <button class="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#templatemo_main_nav" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+            <button class="navbar-toggler border-0" type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#templatemo_main_nav"
+                    aria-controls="templatemo_main_nav"
+                    aria-expanded="false"
+                    aria-label="Abrir menú de navegación">
+                <span class="navbar-toggler-icon" aria-hidden="true"></span>
             </button>
 
-            <div class="align-self-center collapse navbar-collapse flex-fill  d-lg-flex justify-content-lg-between" id="templatemo_main_nav">
+            <div class="align-self-center collapse navbar-collapse flex-fill d-lg-flex justify-content-lg-between" id="templatemo_main_nav">
                 <div class="flex-fill">
-                    <ul class="nav navbar-nav d-flex justify-content-between mx-lg-auto">
+                    <ul class="nav navbar-nav d-flex justify-content-between mx-lg-auto" aria-label="Opciones del menú">
                         <li class="nav-item">
                             <a class="nav-link" href="?">Home</a>
                         </li>
@@ -88,25 +110,41 @@ https://templatemo.com/tm-559-zay-shop
                         </li>
                     </ul>
                 </div>
-                <div class="navbar align-self-center d-flex">
+
+                <div class="navbar align-self-center d-flex" aria-label="Acciones rápidas">
                     <div class="d-lg-none flex-sm-fill mt-3 mb-4 col-7 col-sm-auto pr-3">
                         <div class="input-group">
-                            <input type="text" class="form-control" id="inputMobileSearch" placeholder="Search ...">
-                            <div class="input-group-text">
-                                <i class="fa fa-fw fa-search"></i>
+                            <input type="text"
+                                   class="form-control"
+                                   id="inputMobileSearch"
+                                   placeholder="Buscar..."
+                                   aria-label="Buscar en el sitio (móvil)">
+                            <div class="input-group-text" aria-hidden="true">
+                                <i class="fa fa-fw fa-search" aria-hidden="true"></i>
                             </div>
                         </div>
                     </div>
-                    <a class="nav-icon d-none d-lg-inline" href="#" data-bs-toggle="modal" data-bs-target="#templatemo_search">
-                        <i class="fa fa-fw fa-search text-dark mr-2"></i>
+
+                    <a class="nav-icon d-none d-lg-inline"
+                       href="#"
+                       data-bs-toggle="modal"
+                       data-bs-target="#templatemo_search"
+                       aria-label="Abrir búsqueda">
+                        <i class="fa fa-fw fa-search text-dark mr-2" aria-hidden="true"></i>
                     </a>
-                    <a class="nav-icon position-relative text-decoration-none" href="#">
-                        <i class="fa fa-fw fa-cart-arrow-down text-dark mr-1"></i>
-                        <span class="position-absolute top-0 left-100 translate-middle badge rounded-pill bg-light text-dark">7</span>
+
+                    <a class="nav-icon position-relative text-decoration-none"
+                       href="#"
+                       aria-label="Ver carrito de compras">
+                        <i class="fa fa-fw fa-cart-arrow-down text-dark mr-1" aria-hidden="true"></i>
+                        <span class="position-absolute top-0 left-100 translate-middle badge rounded-pill bg-light text-dark" aria-hidden="true">7</span>
                     </a>
-                    <a class="nav-icon position-relative text-decoration-none" href="#">
-                        <i class="fa fa-fw fa-user text-dark mr-3"></i>
-                        <span class="position-absolute top-0 left-100 translate-middle badge rounded-pill bg-light text-dark">+99</span>
+
+                    <a class="nav-icon position-relative text-decoration-none"
+                       href="#"
+                       aria-label="Ver perfil de usuario">
+                        <i class="fa fa-fw fa-user text-dark mr-3" aria-hidden="true"></i>
+                        <span class="position-absolute top-0 left-100 translate-middle badge rounded-pill bg-light text-dark" aria-hidden="true">+99</span>
                     </a>
                 </div>
             </div>
@@ -116,16 +154,31 @@ https://templatemo.com/tm-559-zay-shop
     <!-- Close Header -->
 
     <!-- Modal -->
-    <div class="modal fade bg-white" id="templatemo_search" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal fade bg-white"
+         id="templatemo_search"
+         tabindex="-1"
+         role="dialog"
+         aria-labelledby="templatemo_search_label"
+         aria-hidden="true">
         <div class="modal-dialog modal-lg" role="document">
             <div class="w-100 pt-1 mb-5 text-right">
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Cerrar búsqueda"></button>
             </div>
+
             <form action="" method="get" class="modal-content modal-body border-0 p-0">
+                <h2 id="templatemo_search_label" class="visually-hidden">Búsqueda</h2>
+
                 <div class="input-group mb-2">
-                    <input type="text" class="form-control" id="inputModalSearch" name="q" placeholder="Search ...">
-                    <button type="submit" class="input-group-text bg-success text-light">
-                        <i class="fa fa-fw fa-search text-white"></i>
+                    <input type="text"
+                           class="form-control"
+                           id="inputModalSearch"
+                           name="q"
+                           placeholder="Buscar..."
+                           aria-label="Buscar en el sitio">
+                    <button type="submit"
+                            class="input-group-text bg-success text-light"
+                            aria-label="Enviar búsqueda">
+                        <i class="fa fa-fw fa-search text-white" aria-hidden="true"></i>
                     </button>
                 </div>
             </form>
--- views/roles/admin/header.view.php
+++ views/roles/admin/header.view.php
@@ -2,7 +2,7 @@
 <html lang="es">
 <head>
 	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>SisWebPhP</title>
 	<link rel="shortcut icon" href="assets/dashboard/assets/img/logo-sena-verde-png-sin-fondo.png">
 
@@ -22,123 +22,205 @@
 	<link rel="stylesheet" href="assets/dashboard/css/sweetalert2.min.css">
 
 	<!-- Sweet Alert V8.13.0 JS file-->
-	<script src="assets/dashboard/js/sweetalert2.min.js" ></script>
+	<script src="assets/dashboard/js/sweetalert2.min.js"></script>
 
 	<!-- jQuery Custom Content Scroller V3.1.5 -->
 	<link rel="stylesheet" href="assets/dashboard/css/jquery.mCustomScrollbar.css">
 
 	<!-- General Styles -->
 	<link rel="stylesheet" href="assets/dashboard/css/style.css">
-
-
 </head>
-<body>
 
+<body>
 	<!-- Main container -->
 	<main class="full-box main-container">
 
 		<!-- Nav lateral -->
-		<section class="full-box nav-lateral">
+		<section class="full-box nav-lateral" aria-label="Menú lateral">
 			<div class="full-box nav-lateral-bg show-nav-lateral"></div>
+
 			<div class="full-box nav-lateral-content">
 				<figure class="full-box nav-lateral-avatar pb-3">
-					<i class="far fa-times-circle show-nav-lateral"></i>
-					<img src="assets/dashboard/assets/avatar/Avatar.png" class="img-fluid" alt="Avatar">
+					<i class="far fa-times-circle show-nav-lateral" aria-hidden="true"></i>
+
+					<img src="assets/dashboard/assets/avatar/Avatar.png" class="img-fluid" alt="Avatar de usuario">
+
 					<figcaption class="roboto-medium text-center">
-						<?php echo $profile->getUserName() . " " . $profile->getUserLastName() ?> <br><small class="roboto-condensed-light">Código Usuario: <?php echo $profile->getUserCode() ?></small>
+						<?php echo $profile->getUserName() . " " . $profile->getUserLastName(); ?>
+						<br>
+						<small class="roboto-condensed-light">
+							Código Usuario: <?php echo $profile->getUserCode(); ?>
+						</small>
 					</figcaption>
 				</figure>
-				<nav class="full-box nav-lateral-menu">
+
+				<nav class="full-box nav-lateral-menu" aria-label="Opciones de perfil">
 					<ul>
 						<li>
-							<a href="#"><i class="fas fa-user fa-fw"></i> &nbsp; Editar Perfil</a>
+							<a href="#">
+								<i class="fas fa-user fa-fw" aria-hidden="true"></i>
+								&nbsp; Editar Perfil
+							</a>
 						</li>
 					</ul>
 				</nav>
+
 				<div class="full-box nav-lateral-bar"></div>
-				<nav class="full-box nav-lateral-menu">
+
+				<nav class="full-box nav-lateral-menu" aria-label="Opciones principales">
 					<ul>
 						<li>
-							<a href="?c=Dashboard" class=""><i class="fab fa-dashcube fa-fw"></i> &nbsp; <?php echo ucfirst($session) ?></a>
+							<a href="?c=Dashboard">
+								<i class="fab fa-dashcube fa-fw" aria-hidden="true"></i>
+								&nbsp; <?php echo ucfirst($session); ?>
+							</a>
 						</li>
 
 						<li>
-							<a href="#" class="nav-btn-submenu"><i class="fas fa-user-graduate fa-fw"></i> &nbsp; Roles <i class="fas fa-chevron-down"></i></a>
-							<ul class="">
-								<li>
-									<a href="?c=Users&a=rolCreate"><i class="fas fa-plus fa-fw"></i> &nbsp; Registrar Rol</a>
-								</li>
-								<li>
-									<a href="?c=Users&a=rolRead"><i class="fas fa-clipboard-list fa-fw"></i> &nbsp; Consultar Roles</a>
-								</li>
-								<li>
-									<a href="#"><i class="fas fa-search fa-fw"></i> &nbsp; Buscar Rol</a>
-								</li>
-							</ul>
-						</li>
-
-						<li>
-							<a href="#" class="nav-btn-submenu"><i class="fas fa-users fa-fw"></i> &nbsp; Usuarios <i class="fas fa-chevron-down"></i></a>
-							<ul class="">
-								<li>
-									<a href="?c=Users&a=userCreate"><i class="fas fa-plus fa-fw"></i> &nbsp; Registrar Usuario</a>
-								</li>
-								<li>
-									<a href="?c=Users&a=userRead"><i class="fas fa-clipboard-list fa-fw"></i> &nbsp; Consultar Usuarios</a>
-								</li>
-								<li>
-									<a href="#"><i class="fas fa-search fa-fw"></i> &nbsp; Buscar Usuario</a>
-								</li>
-							</ul>
-						</li>
-
-						<li>
-							<a href="#" class="nav-btn-submenu"><i class="fas fa-list fa-fw"></i> &nbsp; Categorías <i class="fas fa-chevron-down"></i></a>
+							<a href="#" class="nav-btn-submenu">
+								<i class="fas fa-user-graduate fa-fw" aria-hidden="true"></i>
+								&nbsp; Roles
+								<i class="fas fa-chevron-down" aria-hidden="true"></i>
+							</a>
 							<ul>
 								<li>
-									<a href="#"><i class="fas fa-plus fa-fw"></i> &nbsp; Registrar Categoría</a>
+									<a href="?c=Users&a=rolCreate">
+										<i class="fas fa-plus fa-fw" aria-hidden="true"></i>
+										&nbsp; Registrar Rol
+									</a>
 								</li>
 								<li>
-									<a href="#"><i class="fas fa-clipboard-list fa-fw"></i> &nbsp; Constular Categoría</a>
+									<a href="?c=Users&a=rolRead">
+										<i class="fas fa-clipboard-list fa-fw" aria-hidden="true"></i>
+										&nbsp; Consultar Roles
+									</a>
 								</li>
 								<li>
-									<a href="#"><i class="fas fa-search-dollar fa-fw"></i> &nbsp; Buscar Categoría</a>
+									<a href="#">
+										<i class="fas fa-search fa-fw" aria-hidden="true"></i>
+										&nbsp; Buscar Rol
+									</a>
 								</li>
 							</ul>
 						</li>
 
 						<li>
-							<a href="#" class="nav-btn-submenu"><i class="fas fa-gifts fa-fw"></i> &nbsp; Productos <i class="fas fa-chevron-down"></i></a>
+							<a href="#" class="nav-btn-submenu">
+								<i class="fas fa-users fa-fw" aria-hidden="true"></i>
+								&nbsp; Usuarios
+								<i class="fas fa-chevron-down" aria-hidden="true"></i>
+							</a>
 							<ul>
 								<li>
-									<a href="#"><i class="fas fa-plus fa-fw"></i> &nbsp; Registrar Producto</a>
+									<a href="?c=Users&a=userCreate">
+										<i class="fas fa-plus fa-fw" aria-hidden="true"></i>
+										&nbsp; Registrar Usuario
+									</a>
 								</li>
 								<li>
-									<a href="#"><i class="fas fa-clipboard-list fa-fw"></i> &nbsp; Consultar Productos</a>
+									<a href="?c=Users&a=userRead">
+										<i class="fas fa-clipboard-list fa-fw" aria-hidden="true"></i>
+										&nbsp; Consultar Usuarios
+									</a>
 								</li>
 								<li>
-									<a href="#"><i class="fas fa-search fa-fw"></i> &nbsp; Buscar Producto</a>
+									<a href="#">
+										<i class="fas fa-search fa-fw" aria-hidden="true"></i>
+										&nbsp; Buscar Usuario
+									</a>
 								</li>
 							</ul>
 						</li>
 
 						<li>
-							<a href="#" class="nav-btn-submenu"><i class="fas fa-file-invoice-dollar fa-fw"></i> &nbsp; Ventas <i class="fas fa-chevron-down"></i></a>
+							<a href="#" class="nav-btn-submenu">
+								<i class="fas fa-list fa-fw" aria-hidden="true"></i>
+								&nbsp; Categorías
+								<i class="fas fa-chevron-down" aria-hidden="true"></i>
+							</a>
 							<ul>
 								<li>
-									<a href="#"><i class="fas fa-plus fa-fw"></i> &nbsp; Registrar Venta</a>
+									<a href="#">
+										<i class="fas fa-plus fa-fw" aria-hidden="true"></i>
+										&nbsp; Registrar Categoría
+									</a>
 								</li>
 								<li>
-									<a href="#"><i class="fas fa-clipboard-list fa-fw"></i> &nbsp; Consultar Ventas</a>
+									<a href="#">
+										<i class="fas fa-clipboard-list fa-fw" aria-hidden="true"></i>
+										&nbsp; Consultar Categoría
+									</a>
 								</li>
 								<li>
-									<a href="#"><i class="fas fa-search fa-fw"></i> &nbsp; Buscar Venta</a>
+									<a href="#">
+										<i class="fas fa-search-dollar fa-fw" aria-hidden="true"></i>
+										&nbsp; Buscar Categoría
+									</a>
 								</li>
 							</ul>
 						</li>
 
 						<li>
-							<a href="?"><i class="fas fa-store-alt fa-fw"></i> &nbsp; Empresa</a>
+							<a href="#" class="nav-btn-submenu">
+								<i class="fas fa-gifts fa-fw" aria-hidden="true"></i>
+								&nbsp; Productos
+								<i class="fas fa-chevron-down" aria-hidden="true"></i>
+							</a>
+							<ul>
+								<li>
+									<a href="#">
+										<i class="fas fa-plus fa-fw" aria-hidden="true"></i>
+										&nbsp; Registrar Producto
+									</a>
+								</li>
+								<li>
+									<a href="#">
+										<i class="fas fa-clipboard-list fa-fw" aria-hidden="true"></i>
+										&nbsp; Consultar Productos
+									</a>
+								</li>
+								<li>
+									<a href="#">
+										<i class="fas fa-search fa-fw" aria-hidden="true"></i>
+										&nbsp; Buscar Producto
+									</a>
+								</li>
+							</ul>
+						</li>
+
+						<li>
+							<a href="#" class="nav-btn-submenu">
+								<i class="fas fa-file-invoice-dollar fa-fw" aria-hidden="true"></i>
+								&nbsp; Ventas
+								<i class="fas fa-chevron-down" aria-hidden="true"></i>
+							</a>
+							<ul>
+								<li>
+									<a href="#">
+										<i class="fas fa-plus fa-fw" aria-hidden="true"></i>
+										&nbsp; Registrar Venta
+									</a>
+								</li>
+								<li>
+									<a href="#">
+										<i class="fas fa-clipboard-list fa-fw" aria-hidden="true"></i>
+										&nbsp; Consultar Ventas
+									</a>
+								</li>
+								<li>
+									<a href="#">
+										<i class="fas fa-search fa-fw" aria-hidden="true"></i>
+										&nbsp; Buscar Venta
+									</a>
+								</li>
+							</ul>
+						</li>
+
+						<li>
+							<a href="?">
+								<i class="fas fa-store-alt fa-fw" aria-hidden="true"></i>
+								&nbsp; Empresa
+							</a>
 						</li>
 					</ul>
 				</nav>
@@ -147,14 +229,16 @@
 
 		<!-- Page content -->
 		<section class="full-box page-content">
-			<nav class="full-box navbar-info">
-				<a href="#" class="float-left show-nav-lateral">
-					<i class="fas fa-exchange-alt"></i>
+			<nav class="full-box navbar-info" aria-label="Barra superior del panel">
+				<a href="#" class="float-left show-nav-lateral" aria-label="Mostrar u ocultar menú lateral">
+					<i class="fas fa-exchange-alt" aria-hidden="true"></i>
 				</a>
-				<a href="#">
-					<i class="fas fa-user-cog"></i>
+
+				<a href="#" aria-label="Configuración de usuario">
+					<i class="fas fa-user-cog" aria-hidden="true"></i>
 				</a>
-				<a href="?c=Logout" class="">
-					<i class="fas fa-power-off"></i>
+
+				<a href="?c=Logout" aria-label="Cerrar sesión">
+					<i class="fas fa-power-off" aria-hidden="true"></i>
 				</a>
 			</nav>
