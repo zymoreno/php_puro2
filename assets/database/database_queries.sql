d2d4d86538b06fff5d9e6f1227291e2608172705
zymoreno <zaida81@gmail.com>
2025-12-14 11:31:35 -0500

    database

--- assets/database/database_queries.sql
+++ assets/database/database_queries.sql
@@ -1,3 +1,6 @@
+INSERT INTO `users` (`rol_code`, `user_code`, `user_name`, `user_lastname`, `user_id`, `user_email`, `user_pass`, `user_state`) VALUES
+(1, null, 'Andres', 'Calamardo', '254646485', 'andresc@correo.co', '8cb2237d0679ca88db6464eac60da96345513964', 1);
+
 select 
     r.rol_code,
     r.rol_name,
